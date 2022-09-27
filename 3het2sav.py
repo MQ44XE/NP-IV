a5d44a57d60811442cf31fd6d2c6dd8677abb9c4
mq44xe <mate.ruzsovits@stud.uni-corvinus.hu>
2022-09-27 11:16:53 +0200

    3het2sav final

--- 3het2sav.py
+++ 3het2sav.py
@@ -27,8 +27,7 @@ kocsi_2 = Kocsi("Piros", 2017, 2500, "Sedan")
 
 print(kocsi_1)
 
-
-print(kocsi_1.futott_km)
+"""print(kocsi_1.futott_km)
 print(Kocsi.negy_kereke_van)
 print(Kocsi.szamlalo)
 print(kocsi_2.kiir_szin())
@@ -36,3 +35,51 @@ print()
 kocsi_1.fut(1000)
 print(kocsi_1.futott_km)
 print(kocsi_2.futott_km)
+print()"""
+
+class RandomBag:
+
+    def __init__(self):
+        self.content = []
+
+    def __str__(self):
+        return "[RandomBag] : " + str(self.content)
+
+    def __ad__(self, other):
+        tmp = []
+
+        for item in self.content:
+            tmp.append(item)
+        for item in other.content:
+            tmp.append(item)
+        return tmp
+
+    def put(self, object):
+        self.content.append(object)
+
+    def pop(self):
+        if len(self.content) > 0:
+            index = np.random.randint(0, len(self.content))
+            item = self.content[index]
+            del self.content[index]
+            return item
+        else:
+            return "Bag is empty"
+
+    def size(self):
+        return len(self.content)
+
+bag = RandomBag()
+bag.put("Alma")
+
+print(bag.size())
+print(bag)
+print(bag.pop())
+print(bag.pop())
+print()
+
+for i in range(1, 11):
+    bag.put(i)
+
+while bag.size() > 0:
+    print(bag.pop())
