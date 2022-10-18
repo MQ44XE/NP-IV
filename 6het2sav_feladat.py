3543c7284325ecc313b18b03100aa5bc6ee5f00d
mq44xe <mate.ruzsovits@stud.uni-corvinus.hu>
2022-10-18 11:21:40 +0200

    6het2sav feladat NOT final

---
+++ 6het2sav_feladat.py
@@ -0,0 +1,27 @@
+import numpy as np
+from scipy.stats import norm
+import matplotlib.pyplot as plt
+
+from np6het2sav import Option
+
+opt = Option("C", 100, "20221215", 1)
+print(opt.calcPayoff(139))
+
+K = 100
+expiry = "20221215"
+C = Option("C", K, expiry, 1)
+P = Option("P", K, expiry, -1)
+
+S = 124.35
+t = 0.23
+vola = 0.12
+
+print(C.calcPrice(S, t, vola) + P.calcPrice(S, t, vola) - S + K)
+
+# prices vs spot
+spots = range(250, 500, 5)
+prices = [C.calcPrice(s, 1, vola) for s in spots]
+pays = [C.calcPayoff(s) for s in spots]
+
+plt.plot(spots, prices, spots, pays)
+plt.show()
