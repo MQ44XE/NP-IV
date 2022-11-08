50e06858cda2eb0a2047d70b7b2fbe2cd1fe9acd
mq44xe <mate.ruzsovits@stud.uni-corvinus.hu>
2022-11-08 11:20:32 +0100

    8het munkaja 6het2sav file-okban, kulon jelolve

--- 6het2sav_feladat.py
+++ 6het2sav_feladat.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 from scipy.stats import norm
 import matplotlib.pyplot as plt
 
@@ -23,5 +24,66 @@ spots = range(250, 500, 5)
 prices = [C.calcPrice(s, 1, vola) for s in spots]
 pays = [C.calcPayoff(s) for s in spots]
 
-plt.plot(spots, prices, spots, pays)
+# plt.plot(spots, prices, spots, pays)
+# plt.show()
+
+# 8. HÉT
+
+# set volatility
+#opt_c.vola = 0.3
+#opt_p.vola = 0.3
+
+# calc price at given spot and time to maturity
+#opt_c.calcPrice(362, 1 / 12)
+#opt_p.calcPrice(362, 1 / 12)
+
+# check put call parity
+S = 362
+t = 1 / 12
+
+
+# Geometriai Brown mozgás
+
+def __init__(self):
+    pass
+
+def generate(S0, mu, sigma, T, N):
+    # dS = mu*S*dt + sigma*S*dWt
+    # S(t+dt) = S(t) * exp( (mu-sigma^2/2)*dt + sigma*sqrt(dt)
+    dt = T/N
+    X = np.exp((mu - sigma**2/2)*dt + sigma * np.random.normal(0, np.sqrt(dt), N))
+    return S0*np.cumprod(X)
+
+sigma = 0.35
+N = 250
+S0 = 100
+spots = generate(S0, 0, sigma, 1, N)
+times = np.arange(0, 1, 1/N)
+# plt.plot(times, spots)
+# plt.show()
+
+opt = Option("C", S0, None, 1)
+
+vola = 0.3
+prices = []
+deltas = []
+for (t,S) in zip(times, spots):
+    price = opt.calcPrice(S, 1-t, vola)
+    delta = opt.calcDelta(S, 1-t, vola)
+    prices.append(price)
+    deltas.append(delta)
+
+plt.plot(times, np.array(prices))
 plt.show()
+
+# Using Dataframes
+
+df = pd.Dataframe({"time":times, "spot":spots})
+
+K = 100
+def calcPrice(row):
+    opt = Option("C", K, None, 1)
+    vola = 0.3
+    return opt.calcPrice(row.spot, 1-row.time, vola)
+
+df["price"] = df.apply(calcPrice, axis=1)
--- np6het2sav.py
+++ np6het2sav.py
@@ -12,7 +12,7 @@ class Option:
         self.strike = strike
         self.expiry = expiry
         self.vola = np.nan
-    def calcPrice(self, S:float, timeToExp:float, vola:float, rate:float):
+    def calcPrice(self, S:float, timeToExp:float, vola:float, rate=0):
         if not np.isnan(vola):
             IV = vola
         else:
@@ -34,6 +34,25 @@ class Option:
             print("expired!")
             return np.nan
 
+    def calcDelta(self, S, timeToExp, vola, rate=0):
+        if not np.isnan(vola):
+            IV = vola
+        else:
+            IV = self.vola if not np.isnan(self.vola) else self.initVola
+        if np.isnan(IV):
+            print("Vola is not set!")
+            return np.nan
+        t = timeToExp
+        if t > 0:
+            d1 = (np.log(S / self.strike) + (rate + IV ** 2 / 2) * t) / (IV * np.sqrt(t))
+            if self.right == 'C':
+                return norm.cdf(d1) * self.pos
+            else:
+                return (norm.cdf(d1) - 1) * self.pos
+        else:
+            print("expired!")
+            return np.nan
+
     def initVola(self):
         self.vola = 0.2
 
@@ -45,6 +64,23 @@ class Option:
         else:
             print("Wrong Option Type")
             return None
+
+# 8. HÉT
+
+    def calcVola(self, S: float, time: float, price, rate=0):
+        # calcs implied vola from market price
+        vola_hi = 0.4
+        while self.calcPrice(S, vola_hi, time, rate) < price:
+            vola_hi *= 2
+        vola_low = vola_hi / 2
+        while abs(vola_hi - vola_low) > 0.0001:
+            vola = 0.5 * (vola_low + vola_hi)
+            price_updated = self.calcPrice(S, time, vola, rate)
+            if price_updated < price:
+                vola_low = vola
+            else:
+                vola_hi = vola
+
 
 
 opt = Option("C", 100, "20221215", 1)
